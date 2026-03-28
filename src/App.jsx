0cafbc32af4d1c492e50f4637900a0a3de3f2bcd
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-28 12:13:14 +0530

    added group card rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,69 @@
+import { useEffect, useState } from "react"
 import GroupCard from "./components/GroupCard"
 
 const App = () => {
 
-  const groups = [
-    {
-    id: 1,
-    name: "Goa Trip",
-    members: ["Avinash", "Rahul", "Priya"],
-    expenses: []
-  },
-  {
-    id: 2,
-    name: "Flat 304",
-    members: ["Amit", "Rohit"],
-    expenses: []
+  const [groups, setGroups] = useState([]);
+  const [groupName, setGroupName] = useState("");
+  const [memberInput, setMemberInput] = useState("");
+  const [members, setMembers] = useState([]);
+
+
+  // local storage
+
+  useEffect(()=>{
+    localStorage.setItem("groups", JSON.stringify(groups))
+  }, [groups])
+  // create group
+  const createGroup = () => {
+    const newGroup = {
+      id: crypto.randomUUID(),
+      name: groupName.trim(),
+      members: members,
+    }
+
+    setGroups((prev) => [...prev, newGroup])
+    setGroupName("")
+    setMemberInput("")
   }
-  ]
+
+  // add members
+
+  const addMembers = () => {
+    if(memberInput.trim() === "") return
+    setMembers((prev)=> [...prev, memberInput])
+    setMemberInput("")
+  }
+
 
   return (
     <div className="min-h-screen bg-gray-900 w-full text-white p-4">
 
     <h1 className="font-semibold text-2xl text-slate-100 text-center">Split<span className="text-green-500 ">Smart</span></h1>
 
+    {/* group input form */}
+
+    <div>
+      <form onSubmit={(e)=> {
+        e.preventDefault()
+      }}>
+        
+        <div className="">
+          <label htmlFor="groupName">Group Name : </label>
+          <input type="text" value={groupName} onChange={(e)=>setGroupName(e.target.value)}  name="groupName" id="groupName" className="border border-indigo-400 rounded-sm" />
+        </div>
+
+        <div>
+          <label htmlFor="">Members:</label>
+          <input type="text" name="" id="" value={memberInput} onChange={(e)=>setMemberInput(e.target.value)} className="border border-indigo-400 rounded-sm" />
+          <button type="button" className="border bg-indigo-500 font-medium px-4 py-2 rounded-xl cursor-pointer" onClick={addMembers}>Add Member</button>
+        </div>
+
+        <button type="submit" onClick={createGroup} className="border bg-indigo-500 font-medium px-4 py-2 rounded-xl cursor-pointer">Create</button>
+      </form>
+    </div>
+
+{/* groups rendering */}
     {groups.length === 0 ? (
       <div>
         <p className="mt-6 text-gray-400">No groups yet. Create your first group!</p>
@@ -29,7 +71,9 @@ const App = () => {
     ) : (
       <div className="cards__container grid grid-cols-1 gap-4 mt-4">
       {groups.map((group)=>(
-        <GroupCard key={group.id} group={group}/>
+
+        // group card component
+        <GroupCard key={group.id} group={group} members={members} setMembers={setMembers}/>
       ))}  
     </div>
     )}
--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,9 +1,25 @@
 
-const GroupCard = ( {group} ) => {
+const GroupCard = ( {group, members, setMembers} ) => {
   return (
     <div className='border border-indigo-500 border-l-4 p-2 '>
           <h2 className='text-2xl font-semibold'>{group.name}</h2>
           <p>{group.members.length} members</p>
+
+          {/* members */}
+          <div className="flex gap-2">
+
+          {group.members.map((member, index)=>(
+            <div key={index} className="px-3 py-1 bg-indigo-600 rounded-full flex items-center justify-center gap-2">
+              <span className="">{member}</span>
+              <button onClick={()=>{
+                const updated = members.filter((_, i)=> i != index);
+                setMembers(updated)
+              }}>
+
+              </button>
+            </div>
+            ))}
+            </div>
         </div>
   )
 }
